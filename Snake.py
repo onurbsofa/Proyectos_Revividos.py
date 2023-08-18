56bcdc27d3057c054d46c912632eda2a43eb7bfa
BoFo <brunofazio466@gmail.com>
2023-08-18 18:26:04 -0300

    movimiento de la snake

--- Snake.py
+++ Snake.py
@@ -11,6 +11,7 @@ snake.center = get_random_position()
 length = 1
 snake_body_segments = [snake.copy()]
 snake_direction = (0, 0)
+time, time_steps = 0, 110
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()# frame rate
 
@@ -19,7 +20,22 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                snake_direction = (0, -TILE_SIZE)
+            if event.key == pg.K_DOWN:
+                snake_direction = (0, TILE_SIZE)
+            if event.key == pg.K_LEFT:
+                snake_direction = (-TILE_SIZE, 0)
+            if event.key == pg.K_RIGHT:
+                snake_direction = (TILE_SIZE, 0)
     screen.fill('black')
     [pg.draw.rect(screen, 'green', segment) for segment in snake_body_segments]
+    time_now = pg.time.get_ticks()
+    if time_now - time > time_steps:
+        time = time_now
+        snake.move_ip(snake_direction)
+        snake_body_segments.append(snake.copy())
+        snake_body_segments = snake_body_segments[-length:]
     pg.display.flip()
     clock.tick(60)
